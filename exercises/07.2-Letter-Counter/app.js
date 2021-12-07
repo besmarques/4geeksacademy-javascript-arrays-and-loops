1c7bd4d636020d211a9415952a6315d1828660b6
Bruno Marques <besmarques@gmail.com>
2021-12-07 14:35:08 +0000

    Correct until exercise 8.2

--- exercises/07.2-Letter-Counter/app.js
+++ exercises/07.2-Letter-Counter/app.js
@@ -1,6 +1,21 @@
 let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 let counts = {};
+var letter = "";
 
-// your code here
+for(var i in par){
+	letter = par[i].toLowerCase();
+  if(letter == " "){continue}
+    else if(counts[letter] == undefined){
+    	//console.log("Undefined");
+      counts[letter] = 1;
+    }else{
+    		//console.log("what else");
+        counts[letter] = counts[letter] + 1;
+    }
+  
+}
+
+
+
 
 console.log(counts);
--- exercises/07.3-Flip-Array/app.js
+++ exercises/07.3-Flip-Array/app.js
@@ -1,3 +1,9 @@
 var arr = [45,67,87,23,5,32,60];
+var arr2 = [];
 
 //Your code here.
+for (let i = arr.length; i >= 0 ; i--){
+    arr2.push(arr[i]);
+    //console.log(arr2);
+}
+console.log(arr2);
