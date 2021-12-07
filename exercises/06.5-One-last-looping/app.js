4ce1e10cd5e14b577ef81c760d99603e16624fb3
Bruno Marques <besmarques@gmail.com>
2021-12-07 11:19:05 +0000

    Correct until exercise 6.5

--- exercises/06.5-One-last-looping/app.js
+++ exercises/06.5-One-last-looping/app.js
@@ -1,3 +1,9 @@
 let mySampleArray = ['Esmeralda', 'Kiko', 'Ruth', 'Lebron', 'Pedro', 'Maria', 'Lou', 'Fernando', 'Cesco', 'Bart', 'Annie'];
 
 //your code here
+var arr = [];
+for(var i = mySampleArray.length - 1; i >= 0 ; i = i - 1)
+{
+    arr.push(mySampleArray[i]);
+    console.log(mySampleArray[i]);
+}
